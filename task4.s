9cb5d17287d29a8a809af48e475320dddf507119
Myrah Hussain <myrahhussain05@gmail.com>
2026-01-29 01:38:37 +0500

    done

--- task4.s
+++ task4.s
@@ -3,26 +3,25 @@
 
 main:
     li x5, 5
-    li x6, 5
-    li x10, 0x300
+    li x6, 4
     li x7, 0
+    li x10, 0x200
 
 L1:
-    bge x7, x5, done
+    bge x7, x5, end
     li x29, 0
 
 L2:
-    bge x29, x6, next
-    slli t0, x29, 2
-    add t1, x10, t0
-    add t2, x7, x29
-    sw t2, 0(t1)
+    slli x31, x29, 2
+    add x12, x7, x29
+    add x13, x10, x31
+    sw x12, 0(x13)
     addi x29, x29, 1
-    j L2
+    blt x29, x6, L2
 
 next:
     addi x7, x7, 1
     j L1
 
-done:
-    j done
+end:
+    j end
